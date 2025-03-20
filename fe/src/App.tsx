3e063bf5eef7f8a0cd83e46138030d09102a63eb
Priyanshudotdev <priyanshu.kayarkar009@gmail.com>
2025-03-21 00:01:57 +0530

    FeedBack Page

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -7,27 +7,32 @@ import Notifications from "./pages/notification";
 import Navbar from "./pages/navbar";
 import Login from "./pages/login";
 import Settings from "./pages/setting";
+import FeedbackPage from "./pages/feedback";
 
 const Feedback = lazy(() => import("./pages/feedback"));
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}  />
-          <Route path="/feedback" element={
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/feedback"
+          element={
             <Suspense fallback={<Loader />}>
               <Feedback />
             </Suspense>
-            } />
-            <Route path="/header" element={<Header/>}></Route>
-            <Route path="/notification" element={<Notifications/>}></Route>
-            <Route path="/navbar" element={<Navbar/>}></Route>
-            <Route path="/login" element={<Login/>}></Route>
-            <Route path="/setting" element={<Settings/>}></Route>
-        </Routes>
-      </Router>   
-  )
+          }
+        />
+        <Route path="/header" element={<Header />}></Route>
+        <Route path="/notification" element={<Notifications />}></Route>
+        <Route path="/navbar" element={<Navbar />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/feedback" element={<FeedbackPage />}></Route>
+        <Route path="/setting" element={<Settings />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
